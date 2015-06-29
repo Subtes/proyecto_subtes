e3d91763ddd073f3c02d1f5662bcafac51220b8d
Mauricio <fdefelippe@gmail.com>
2015-06-29 16:24:05 -0300

    Desktop with 4 SCreen, ATP-Center-Hadware-Right

--- CAF6000/main.cpp
+++ CAF6000/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QDebug>
+#include <QDesktopWidget>
 
 #include "boardcenter.h"
 #include "boardhardware.h"
@@ -22,15 +23,46 @@ int main(int argc, char *argv[])
 
     // VIEWS
     BoardHardware *m_h = new BoardHardware(0,m_subte,m_eventHandler);
-    m_h->show();
     BoardCenter * m_c = new BoardCenter(0,m_subte,m_eventHandler);
-    m_c->show();
-    //BoardLeft *m_l = new BoardLeft(0,m_subte,m_eventHandler);
-    //m_l->show();
-    //BoardRight *m_r = new BoardRight(0,m_subte,m_eventHandler);
-    //m_r->show();
+    BoardLeft *m_l = new BoardLeft(0,m_subte,m_eventHandler);
+    BoardRight *m_r = new BoardRight(0,m_subte,m_eventHandler);
     //BoardTop *m_t = new BoardTop(0,m_subte,m_eventHandler);
-    //m_t->show();
+
+    QDesktopWidget *desktop = a.desktop();
+    if(desktop->screenCount() == 4){
+        qDebug() << "Entre en For de pantallas igual 4: ";
+        for(int i = 0; i < desktop->screenCount(); i++){
+            qDebug() << "Dimensiones --> "<< desktop->screenGeometry(i);
+        }
+
+    QRect s0 = desktop->screenGeometry(0);
+    QRect s1 = desktop->screenGeometry(1);
+    QRect s2 = desktop->screenGeometry(2);
+    QRect s3 = desktop->screenGeometry(3);
+
+    /**
+     * Dimensiones -->  QRect(0,0 1024x768)
+     * Dimensiones -->  QRect(-2048,0 1024x768)
+     * Dimensiones -->  QRect(1024,0 1024x768)
+     * Dimensiones -->  QRect(-1024,0 1024x768)
+     */
+
+    m_h->move(s0.topLeft());
+    m_l->move(s1.topLeft());
+    m_r->move(s2.topLeft());
+    m_c->move(s3.topLeft());
+
+    m_h->showNormal();
+    m_l->showNormal();
+    m_c->showNormal();
+    m_r->showNormal();
+
+    }else{
+        m_h->showNormal();
+        m_l->showNormal();
+        m_c->showNormal();
+        m_r->showNormal();
+    }
 
     // DEPENDENCY INJECTION
     m_eventHandler->setModel(m_subte);
